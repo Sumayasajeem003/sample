f64067b6c0bb5c2cd17f334b3341d9b61e9ac71b
Sumayasajeem003 <sumayasajeem@gmail.com>
2026-05-04 18:16:32 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,33 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Visiting from './components/cls_fun'
+import Counterss from './components/cls_usestate2'
+import FormList from './components/formandlist'
+import Counting from './components/fun_usestate'
+
+import Message from './components/message_box'
+import MultiInputForm from './components/Multipleinputform'
+import SimpleForm from './components/Nameform'
+import ProductList from './components/productlist'
+import FruitList from './components/simplelist'
+import Userlist from './components/userlist'
+import Product from './components/product_display'
+import Apps from './bootstrap'
+import Visit from './components/Cls'
+import Counters from './components/Cls_usestate'
+import Clarify from './components/external_style'
+import Func from './components/func'
+import Greeting from './components/styling'
+import Counter from './components/use_state'
+import Greet from './inline_style'
+import Navs from './components/cake'
+import BasicExample from './components/cake_cards'
+import GreetngCard from './components/greeting_card'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    {/* <h1>Home Page</h1>
+ <Func name='Cybersquare'/>
+    <Visit name="python,flutter,mern,php" details='we are providing it training and placement'/>
+    <Counter/>
+    <Counters/>
+    <Greeting/>
+    <Greet/>
+    <Clarify/>
+    <Apps/> */}
+    {/* <Navs/>
+    <BasicExample/> */}
+    {/* <GreetngCard name='Sumaya'/> */}
+    {/* <Product name='Sumaya' email='sumayasajee@gmail.com' phone='9037055252'/> */}
+    
+    {/* <Message msg='Email' msg1='you have a new message'/> */}
+     <Visiting name='sumu'/>
+    <Func name='cyber'/>
+    <Counting/>
+    <Counterss/>
+    <SimpleForm/>
+    <MultiInputForm/>
+    <FruitList/>
+    <Userlist/>
+    <FormList/>
+    <ProductList/>
+    
+  
+      
     </>
   )
 }
